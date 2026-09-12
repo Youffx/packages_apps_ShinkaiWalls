0c45fd8b356ef51743141a19fe51cc7ac1b629fa
Youff <asa.jazal@gmail.com>
2026-09-12 16:17:25 +0000

    fix: overlay blocks touches during animation, remove replay on refresh
    Set overlay clickable/focusable=true so taps don't pass through
    to hidden content. Removed playShapesAnimation from refresh listener
    so animation only plays once on app launch.
    
    Signed-off-by: Youffx <asa.jazal@gmail.com>

--- src/com/shinkai/wallpapers/MainActivity.kt
+++ src/com/shinkai/wallpapers/MainActivity.kt
@@ -55,7 +55,6 @@ class MainActivity : AppCompatActivity() {
         )
         swipeRefresh.setOnRefreshListener {
         fetchWallpapersOnline()
-        playShapesAnimation()
         }
 
         fetchWallpapersOnline()
